2f17a470a0d47a065fa96201eefc1d32d8f8c587
alexeypav <adpavlenko@gmail.com>
2016-11-20 21:28:14 +1300

    Add files via upload

---
+++ GetUserPerms.ps1
@@ -0,0 +1,22 @@
+﻿#Find folders that a group has access perms on, Checks if inherited
+
+#Variables
+$folderRoot = "\\server\share"
+$group = "Authenticated Users"
+$output = "C:\$group Perms List.txt"
+
+#Script
+$folders = Get-ChildItem $folderRoot -Recurse | ?{ $_.PSIsContainer } | Select-Object -ExpandProperty fullname
+$requested = @()
+
+#List All Folders in console (optional)
+$folders.foreach({$_})
+
+#Check Perms and Copy to File
+foreach ($folder in $folders){
+
+    $access = ((Get-Acl $folder).Access)
+
+    ($access.ForEach({if ($_.IdentityReference -like  "*$group*") {"`"$folder`" contains $group, Inherited: " + $_.IsInherited}})).foreach({ $_ | out-file $output -Append})
+
+} 

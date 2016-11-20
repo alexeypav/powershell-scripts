fb7d54397b58acf4b476035f4ccb5c829b8bb862
alexeypav <adpavlenko@gmail.com>
2016-11-20 21:35:14 +1300

    Add files via upload

---
+++ GetNotices.ps1
@@ -0,0 +1,52 @@
+﻿<# Script to get KAMAR notices for wallflower
+created to replace the old VBS and Batch scripts
+#>
+
+#Enter Server address for notice
+$address = "http://address"
+
+
+
+
+#Bellow shouldn't be modified
+
+#Get parent directory, assuming the script is located under wallflowe\scripts
+$Parent = (get-item $PSScriptRoot ).parent.FullName
+
+#Path to Template HTML/Javascript files
+$footers = Get-Content $Parent\inputfiles\static\footers.txt 
+$headers = Get-Content $Parent\inputfiles\static\headers.txt 
+
+
+
+#removes previous content
+Remove-Item $Parent\content.html -force
+Remove-Item $PSScriptRoot\notices.txt
+
+
+#Gets notices and outputs to temp file
+& "$PSScriptRoot\wget.exe" "$address" -O "$PSScriptRoot\notices.txt"
+
+#inputs HTML/javascript content from Headers files to start of content.html
+
+foreach ($line in $headers ){   Out-File $Parent\content.html -InputObject $line -Append}
+
+#copies select HTML content from notices file to content.html
+$flag=0
+Get-Content $PSScriptRoot\notices.txt |
+foreach {
+    Switch -Wildcard ($_){
+        '*<div id="notices">*' {$flag=1}
+        '*<div id="footer">*' {$flag=0}
+    }
+    if ($flag -eq 1){
+        Out-File $Parent\content.html -InputObject $_ -Append
+    }
+}
+
+#inputs HTML content from Footers files to end of content.html
+
+foreach ($line in $footers ){   Out-File $Parent\content.html -InputObject $line -Append}
+
+#Optional, Copies content.html to IIS publish folder assuming the script isn't located there (Sometimes running the script from wwwroot causes issues)
+#Copy-Item $Parent\content.html -destination  C:\inetpub\wwwroot\wallflower\content.html -Force
---
+++ TicketFeed.ps1
@@ -0,0 +1,93 @@
+﻿#Get latest gmail emails and output in HTML Table
+#AlexeyP 2016
+
+#Set Variables
+$username = "username@googleappsdomain.com"
+$password = "password"
+
+#Number of emails to show
+$NumberOfEntries = 9
+
+$time = get-date -Format t
+
+#Connect to gmail using webclient, using https://mail.google.com/mail/feed/atom 
+function get-emails($a, $b){
+
+    $webclient = new-object System.Net.WebClient
+    $webclient.Credentials = new-object System.Net.NetworkCredential ("$a","$b")
+    [xml]$xml= $webclient.DownloadString("https://mail.google.com/mail/feed/atom")
+    $xml
+
+}
+
+
+[xml]$xml = get-emails $username $password
+ 
+
+$Tickets = @{}
+ 
+ 
+function check-latestticket {
+ 
+    [datetime]$1stTime = $xml.feed.entry[0].modified
+ 
+    if($1stTime -gt (Get-Date).AddMinutes("-10")){
+        $true
+    }else{
+        $false
+    }
+}
+ 
+#Outputs an html table with added variables from section above
+function html-display {
+ 
+    #Put a flashing alert if latest email less than 5minutes old
+    if (check-latestticket -eq $true){
+ 
+         "<section class=`"contact-info`">
+          <span class=`"blink`" style=`"color:red`">New Ticket:" + $xml.feed.entry[0].title + "</span>
+       </section>"
+ 
+ 
+    }else{
+    '<section class="contact-info">
+          <span class="blink"></span>
+       </section>'
+ 
+ 
+    }
+ 
+ 
+    "<table border=1 frame=void rules=rows >
+  <colgroup><col/><col/><col/><col/><col/><col/></colgroup>
+  <tr><th>Title</th><th>Summary</th><th>From</th><th>When</th></tr>"
+ 
+   foreach ($number in 0..($NumberOfEntries -1))
+    {
+        if ($xml.feed.entry[$number].summary.length -lt 80)
+        {
+            $summary = $xml.feed.entry[$number].summary
+        }
+        else
+        {
+            $summary = $xml.feed.entry[$number].summary.substring(0,80) + "..."
+        }
+ 
+        "<tr><td>" + $xml.feed.entry[$number].title + "</td><td>" + $summary + "</td><td>" + $xml.feed.entry[$number].author.name + "</td><td>" + ([datetime]$xml.feed.entry[$number].modified).ToString("dddd dd/MM/yy hh:mm tt") + "</td></tr>"
+ 
+    }
+   
+   "</table>
+    
+        <br><br><br><br>
+    <div align=`"center`">
+  Current Time: $time
+</div>
+    
+    "
+ 
+}
+ 
+ 
+#Output html
+html-display
